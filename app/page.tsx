0d7b1443e798f5117ba6964f995f2e8ca986d19c
dash000meyti <dashmeyti000@gmail.com>
2026-05-04 08:22:50 +0330

    :D - Meyti : ساخت پروژه جدید نکست

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <span>Hello World</span>
+      </main>
+    </div>
+  );
+}
